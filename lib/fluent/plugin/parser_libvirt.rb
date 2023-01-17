b4b9673796a47e8b400adcb77e3c1130915672f3
thaim <thaim24@gmail.com>
2023-01-17 22:51:40 +0000

    parse simple qemu exec log

--- lib/fluent/plugin/parser_libvirt.rb
+++ lib/fluent/plugin/parser_libvirt.rb
@@ -5,9 +5,62 @@ module Fluent
     class LibvirtParser < Fluent::Plugin::Parser
       Fluent::Plugin.register_parser("libvirt", self)
 
-      config_set_default :time_key, "0"
+      config_set_default :time_format, '%Y-%m-%d %H:%M:%S.%L%z'.freeze
+      config_set_default :time_key, "time"
 
       def parse(text)
+        text.split("\n").each do |line|
+          # time = "2023-01-16 13:00:00.500+0000"
+          # record = {
+          #   'msg' => 'starting up',
+          #   'versions' => {
+          #     'libvert version' => '8.0.0',
+          #     'package' => '1ubuntu7.1 (Christian Ehrhardt <christian.ehrhardt@canonical.com> Thu, 19 May 2022 08:14:48 +0200)',
+          #     'qemu version' => '6.2.0Debian 1:6.2+dfsg-2ubuntu6.3',
+          #     'kernel' => '5.15.0-41-generic',
+          #     'hostname' => 'devserver'
+          #   },
+          #   'env' => {'LC_ALL': 'C'},
+          #   'command' => '/usr/bin/qemu-system-x86_64 -name guest=testvm,debug-threads=on -S -msg timestamp=on'
+          # }
+
+          values = line.split("\\")
+          raw_record = {}
+          status = "start"
+          values.each_with_index do |value, index|
+            if status == "start"
+              if value.include?(": starting up ")
+                raw_record['time'] = value.split(": starting up ")[0]
+                raw_record['msg'] = "starting up"
+                raw_record['versions'] = {}
+                versions = value.split(": starting up ")[1]
+              end
+              versions.split(',').each do |version|
+                k,v = version.split(':',2)
+                raw_record['versions'][k.strip] = v.strip
+              end
+
+
+              status = "env"
+            elsif status == "env" and value =~ /[a-zA-Z_]*=[a-zA-Z0-9]*/
+              k, v = value.split('=', 2)
+              if raw_record['env'] == nil
+                raw_record['env'] = {}
+              end
+              raw_record['env'][k] = v.strip
+            elsif status == "env" or status == "command"
+              if raw_record['command'] == nil
+                raw_record['command'] = value
+              else
+                raw_record['command'].concat(value)
+              end
+              status = "command"
+            end
+          end
+          time, record = convert_values(parse_time(raw_record), raw_record)
+
+          yield time, record
+        end
       end
     end
   end
--- test/plugin/test_parser_libvirt.rb
+++ test/plugin/test_parser_libvirt.rb
@@ -11,26 +11,38 @@ class LibvirtParserTest < Test::Unit::TestCase
   CONFIG = {
   }
 
-  test "execute qemu" do
+  test "simple starting up event" do
     d = create_driver('')
-    log = "2023-01-17 13:00:00.500+0000: starting up libvirt version: 8.0.0, package: 1ubuntu7.1 (Christian Ehrhardt <christian.ehrhardt@canonical.com> Thu, 19 May 2022 08:14:48 +0200), qemu version: 6.2.0Debian 1:6.2+dfsg-2ubuntu6.3, kernel: 5.15.0-41-generic, hostname: devserver\nLC_ALL=C \\n/usr/bin/qemu-system-x86_64 \\n-name guest=wnode04,debug-threads=on \\n-S \\n-msg timestamp=on"
-    t = event_time('2023-01-17 13:00:00.500+0000', format: '%Y-%m-%d %H:%M:%S.%L%z')
+    log = "2023-01-17 13:00:00.500+0000: starting up libvirt version: 8.0.0, kernel: 5.15.0-41-generic\\LC_ALL=C \\/usr/bin/qemu-system-x86_64 \\-name guest=testvm,debug-threads=on \\-S \\-msg timestamp=on"
+
+    t = event_time('2023-01-17 13:00:00.500+0000')
     r = {
       'msg' => 'starting up',
       'versions' => {
-        'libvert version' => '8.0.0',
+        'libvirt version' => '8.0.0',
+        'kernel' => '5.15.0-41-generic',
+      },
+      'env' => {'LC_ALL' => 'C'},
+      'command' => '/usr/bin/qemu-system-x86_64 -name guest=testvm,debug-threads=on -S -msg timestamp=on'
+    }
+    d.instance.parse(log) do |time, record|
+      assert_equal t, time
+      assert_equal r, record
+    end
+  end
+
+  test "full starting up event" do
+    log = "2023-01-17 13:00:00.500+0000: starting up libvirt version: 8.0.0, package: 1ubuntu7.1 (Christian Ehrhardt <christian.ehrhardt@canonical.com> Thu, 19 May 2022 08:14:48 +0200), qemu version: 6.2.0Debian 1:6.2+dfsg-2ubuntu6.3, kernel: 5.15.0-41-generic, hostname: devserver|LC_ALL=C |/usr/bin/qemu-system-x86_64 |-name guest=wnode04,debug-threads=on |-S |-msg timestamp=on"
+    r = {
+      'msg' => 'starting up',
+      'versions' => {
+        'libvirt version' => '8.0.0',
         'package' => '1ubuntu7.1 (Christian Ehrhardt <christian.ehrhardt@canonical.com> Thu, 19 May 2022 08:14:48 +0200)',
         'qemu version' => '6.2.0Debian 1:6.2+dfsg-2ubuntu6.3',
         'kernel' => '5.15.0-41-generic',
         'hostname' => 'devserver'
       },
-      'env' => {'LC_ALL': 'C'},
-      'command' => '/usr/bin/qemu-system-x86_64 -name guest=testvm,debug-threads=on -S -msg timestamp=on'
     }
-    d.instance.parse(log) do |time, record|
-      assert_equal time, t
-      assert_equal record, r
-    end
   end
 
   private
